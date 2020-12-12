8b7148e0a1e29722e6abe7349f1c33518122fd3f
Dan Percic <danpercic86@gmail.com>
2020-12-12 17:28:35 +0200

    fix admin, remove unused imports

--- src/administration/admin.py
+++ src/administration/admin.py
@@ -1,5 +1,5 @@
-from django.contrib.admin import register, ModelAdmin
-from django.contrib.auth import admin
+from django.contrib import admin
+from django.contrib.admin import register
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User, ExampleModel
 
