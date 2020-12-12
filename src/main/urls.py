1f7f8fe4cf15d65592b168e18102e4f9ce4ac51a
Dan Percic <danpercic86@gmail.com>
2020-12-12 16:03:10 +0200

    update main/urls.py

--- src/main/urls.py
+++ src/main/urls.py
@@ -18,26 +18,27 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
-from drf_yasg import  openapi
+from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Opportunity API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Opportunity API",
+        default_version='v1',
+        description="Opportunity API description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="dsc.upt@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
+
 urlpatterns = [
     path('', RedirectView.as_view(url='/api/admin/')),
     path('api/admin/', admin.site.urls),
-    path('api/swagger',schema_view.with_ui('swagger',cache_timeout=0),name = 'swagger'),
+    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger'),
 ]
 
 if settings.DEBUG:
