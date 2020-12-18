fb132cd1d52c3278b5a69505b365eacf0ddc191a
Dan Percic <danpercic86@gmail.com>
2020-12-18 20:29:18 +0200

    fix urls

--- src/main/urls.py
+++ src/main/urls.py
@@ -44,8 +44,6 @@ urlpatterns = [
     path('api/', include(router.urls))
 ]
 
-urlpatterns += router.urls
-
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
