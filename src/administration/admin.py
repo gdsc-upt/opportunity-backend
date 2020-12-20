7ea50652f1952ea071e50e55691ecb14ed1c2e06
Gaita <alex.gaita99@gmail.com>
2020-12-20 17:21:40 +0200

    Added @admin.register decorator to every admin model

--- src/administration/admin.py
+++ src/administration/admin.py
@@ -3,22 +3,32 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin, GroupAdmin
 from django.contrib.auth.models import Group
 from django.utils.html import format_html
 
-from administration.models import User, ExampleModel, Organisation, Partner, Faq, Opportunity, MenuItem, Article, Newsletter, WantToHelp, \
+from administration.models import User, ExampleModel, Organisation, Partner, Faq, Opportunity, MenuItem, Article, \
+    Newsletter, WantToHelp, \
     OpportunityCategory, UserProfile
 from administration.admin_site import admin_site
 
 
+@admin.register(User, site=admin_site)
 class UserAdmin(BaseUserAdmin):
     pass
 
 
-class BaseModelAdmin(admin.ModelAdmin):
-    readonly_fields = ('created','updated')
+@admin.register(Group, site=admin_site)
+class GroupAdmin(GroupAdmin):
+    pass
 
+
+class BaseModelAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+
+@admin.register(ExampleModel, site=admin_site)
 class ExampleModelAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'age')
 
 
+@admin.register(Partner, site=admin_site)
 class PartnerAdmin(BaseModelAdmin):
     list_display = ('name', 'slug', 'website', 'logo', 'is_published')
     list_filter = ('is_published', 'created', 'updated')
@@ -26,6 +36,7 @@ class PartnerAdmin(BaseModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 
+@admin.register(Faq, site=admin_site)
 class FaqAdmin(BaseModelAdmin):
     list_display = ('question', 'answer', 'is_published')
     list_filter = ('is_published', 'created', 'updated')
@@ -33,11 +44,13 @@ class FaqAdmin(BaseModelAdmin):
     list_editable = ('is_published',)
 
 
+@admin.register(Organisation, site=admin_site)
 class OrganisationAdmin(BaseModelAdmin):
     search_fields = ('name',)
     prepopulated_fields = {'slug': ('name',)}
 
 
+@admin.register(Opportunity, site=admin_site)
 class OpportunityAdmin(BaseModelAdmin):
     list_display = ('name', 'show_org_url', 'deadline', 'show_opp_url', 'description')
     list_filter = ('deadline', 'organisation')
@@ -55,6 +68,7 @@ class OpportunityAdmin(BaseModelAdmin):
     show_org_url.short_description = "organisation"
 
 
+@admin.register(MenuItem, site=admin_site)
 class MenuItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'link', 'image', 'parent')
     list_filter = ('parent',)
@@ -62,6 +76,7 @@ class MenuItemAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 
+@admin.register(Article, site=admin_site)
 class ArticleAdmin(BaseModelAdmin):
     list_display = ('title', 'slug', 'image', 'description', 'is_published', 'created')
     list_filter = ('is_published', 'created', 'updated')
@@ -69,6 +84,7 @@ class ArticleAdmin(BaseModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
 
+@admin.register(Newsletter, site=admin_site)
 class NewsletterAdmin(BaseModelAdmin):
     list_display = ('email', 'slug', 'other', 'created', 'updated')
     list_filter = ('created', 'updated')
@@ -76,12 +92,14 @@ class NewsletterAdmin(BaseModelAdmin):
     prepopulated_fields = {'slug': ('email',)}
 
 
+@admin.register(WantToHelp, site=admin_site)
 class WantToHelpAdmin(admin.ModelAdmin):
     list_display = ('name', 'email')
     list_filter = ('name', 'email')
     search_fields = ('email',)
 
 
+@admin.register(OpportunityCategory, site=admin_site)
 class OpportunityCategoryAdmin(BaseModelAdmin):
     list_display = ('name', 'slug', 'created', 'updated')
     list_filter = ('created', 'updated')
@@ -89,22 +107,22 @@ class OpportunityCategoryAdmin(BaseModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 
+@admin.register(UserProfile, site=admin_site)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'organisation', 'description')
     list_filter = ('organisation',)
     search_fields = ('user', 'organization')
 
-
-admin_site.register(ExampleModel, ExampleModelAdmin)
-admin_site.register(User, UserAdmin)
-admin_site.register(Group, GroupAdmin)
-admin_site.register(Partner, PartnerAdmin)
-admin_site.register(Faq, FaqAdmin)
-admin_site.register(Organisation, OrganisationAdmin)
-admin_site.register(Opportunity, OpportunityAdmin)
-admin_site.register(MenuItem, MenuItemAdmin)
-admin_site.register(Article, ArticleAdmin)
-admin_site.register(Newsletter, NewsletterAdmin)
-admin_site.register(WantToHelp, WantToHelpAdmin)
-admin_site.register(OpportunityCategory, OpportunityCategoryAdmin)
-admin_site.register(UserProfile, UserProfileAdmin)
+# admin_site.register(ExampleModel, ExampleModelAdmin)
+# admin_site.register(User, UserAdmin)
+# admin_site.register(Group, GroupAdmin)
+# admin_site.register(Partner, PartnerAdmin)
+# admin_site.register(Faq, FaqAdmin)
+# admin_site.register(Organisation, OrganisationAdmin)
+# admin_site.register(Opportunity, OpportunityAdmin)
+# admin_site.register(MenuItem, MenuItemAdmin)
+# admin_site.register(Article, ArticleAdmin)
+# admin_site.register(Newsletter, NewsletterAdmin)
+# admin_site.register(WantToHelp, WantToHelpAdmin)
+# admin_site.register(OpportunityCategory, OpportunityCategoryAdmin)
+# admin_site.register(UserProfile, UserProfileAdmin)
