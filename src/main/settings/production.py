8a2cc224ba35e4a4dae6128c061ff7afc1cd74b2
Dan Percic <danpercic86@gmail.com>
2020-12-20 23:54:20 +0200

    mark as secure

--- src/main/settings/production.py
+++ src/main/settings/production.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlparse
+
 from .base import *
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
@@ -8,11 +10,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MIDDLEWARE.insert(0, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
+USE_X_FORWARDED_PORT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 SILENCED_SYSTEM_CHECKS = [
     'security.W004',  # SECURE_HSTS_SECONDS
     'security.W008',  # SECURE_SSL_REDIRECT
-    'security.W012',  # SESSION_COOKIE_SECURE
-    'security.W016',  # CSRF_COOKIE_SECURE
 ]
 
 #######################################
