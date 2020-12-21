c812b42dd2e978b650b1cf731da9aa69fe63535b
Gaita <alex.gaita99@gmail.com>
2020-12-21 20:39:51 +0200

    Token Changes Every Time an User Logs In

--- src/administration/views.py
+++ src/administration/views.py
@@ -1,8 +1,12 @@
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.mixins import CreateModelMixin
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
 from administration.models import Organisation, Category, UserProfile, Opportunity
-from administration.serializers import OrganizationSerializer, CategorySerializer, UserProfileSerializer, OpportunitySerializer
+from administration.serializers import OrganizationSerializer, CategorySerializer, UserProfileSerializer, \
+    OpportunitySerializer
 
 
 class OpportunityViewSet(CreateModelMixin, ReadOnlyModelViewSet):
@@ -23,3 +27,20 @@ class CategoryViewSet(ReadOnlyModelViewSet):
 class UserProfileViewSet(ReadOnlyModelViewSet):
     serializer_class = UserProfileSerializer
     queryset = UserProfile.objects.all()
+
+
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+
+        Token.objects.filter(user=user).delete()
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'username': user.username,
+            'email': user.email
+        })
