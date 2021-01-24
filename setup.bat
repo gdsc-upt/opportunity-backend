14cb8b52e0a1430cf164066327025c1d3a51891b
Dan Percic <danpercic86@gmail.com>
2021-01-24 20:05:14 +0200

    update setup.bat for poetry

--- setup.bat
+++ setup.bat
@@ -1,22 +1,25 @@
 @echo off
 
-IF NOT EXIST venv (
-    echo Python system interpreter:
-    where python
-    python --version || goto :error
-    pip install pyyaml
-    pip install poetry
-    echo Checking variables configuration
-    python src/check_config_vars.py || goto :error
-)
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+call set PATH=%PATH%;%USERPROFILE%\.poetry\bin;
+call set PATH=%USERPROFILE%\AppData\Local\Programs\Python\Python39;%PATH%;
+call set PATH=%USERPROFILE%\AppData\Local\Programs\Python\Python39\Scripts;%PATH%;
+echo Python system interpreter:
+where python
+python --version || goto :error
+echo Checking variables configuration
+pip install pyyaml
+python src/check_config_vars.py || goto :error
 
 echo Activating venv
-poetry shell || goto :error
+call poetry shell
+for /f %%p in ('poetry env info --path') do set POETRYPATH=%%p
+call %POETRYPATH%\Scripts\activate.bat
 echo Python version:
 python -VV
 
 echo installing requirements...
-poetry install || goto :error
+call poetry install || goto :error
 
 echo Setting up database...
 python src/manage.py migrate || goto :error
